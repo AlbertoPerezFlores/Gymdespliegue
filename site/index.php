9787a6290d533707d19bad4f9662e6db4c4dd529
Albertoperezflores <5585180@alu.murciaeduca.es>
2023-05-31 19:21:34 +0200

    abre directo el proyecto

--- site/index.php
+++ site/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "<h1>Esto es una prueba</h1>";
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
